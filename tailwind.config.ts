98cafcf17366408155fdefd1913c9e8200ab0866
Shubham Tiwary <shubhut17@gmail.com>
2024-01-21 10:39:32 +0530

    next-app created and basic styling setup

---
+++ tailwind.config.ts
@@ -0,0 +1,36 @@
+import type { Config } from "tailwindcss";
+/** @type {import('tailwindcss').Config} */
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    colors: {
+      primary: {
+        100: "#ffffff",
+      },
+      dark: {
+        100: "#000000",
+      },
+      "accent-blue": "#7D6DD8",
+    },
+    fontFamily: {
+      inter: ["var(--font-inter)"],
+    },
+    plugins: [
+      require("tailwindcss-animate"),
+      require("@tailwindcss/typography"),
+    ],
+  },
+};
+export default config;
