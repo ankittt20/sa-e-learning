a6c044078e373373997cfa65add9866a4639c279
Shubham Tiwary <shubhut17@gmail.com>
2024-03-28 23:10:18 +0530

    account settings page created

--- components/shared/admin/Sidebar.tsx
+++ components/shared/admin/Sidebar.tsx
@@ -6,10 +6,10 @@ type Props = {};
 
 const Sidebar = (props: Props) => {
   return (
-    <div className="flex flex-col px-8 py-16 items-center justify-center text-center">
-      <h1 className="text-3xl font-bold mb-20">SAelearning</h1>
-      <div className="space-y-5">
-        <Button className="space-x-3 bg-accent-blue rounded-lg w-40 h-12">
+    <div className="flex flex-col items-center justify-center bg-[#F3F1FC] px-8 py-16 text-center">
+      <h1 className="mb-20 text-3xl font-bold">SAelearning</h1>
+      <div className="flex-center flex flex-col space-y-5">
+        <Button className="h-12 w-40 space-x-3 rounded-lg bg-accent-blue">
           <Image
             src="/assets/icons/dashboard-icon.svg"
             alt="Dashboard"
@@ -18,7 +18,7 @@ const Sidebar = (props: Props) => {
           />
           <p className="text-primary-100">Dashboard</p>
         </Button>
-        <Button className="space-x-3 bg-primary-100 rounded-lg w-40 h-12">
+        <Button className="h-12 w-40 space-x-3 rounded-lg bg-primary-100">
           <Image
             src="/assets/icons/calendar-icon.svg"
             alt="Calendar"
@@ -27,7 +27,7 @@ const Sidebar = (props: Props) => {
           />
           <p className="text-[#888888]">Calender</p>
         </Button>
-        <Button className="space-x-3 bg-primary-100 rounded-lg w-40 h-12">
+        <Button className="h-12 w-40 space-x-3 rounded-lg bg-primary-100">
           <Image
             src="/assets/icons/courses-icon.svg"
             alt="Courses"
@@ -36,7 +36,7 @@ const Sidebar = (props: Props) => {
           />
           <p className="text-[#888888]">Courses</p>
         </Button>
-        <Button className="space-x-3 bg-primary-100 rounded-lg w-40 h-12">
+        <Button className="h-12 w-40 space-x-3 rounded-lg bg-primary-100">
           <Image
             src="/assets/icons/messages-icon.svg"
             alt="Messages"
@@ -45,7 +45,7 @@ const Sidebar = (props: Props) => {
           />
           <p className="text-[#888888]">Messages</p>
         </Button>
-        <Button className="space-x-3 bg-primary-100 rounded-lg w-40 h-12">
+        <Button className="h-12 w-40 space-x-3 rounded-lg bg-primary-100">
           <Image
             src="/assets/icons/settings-icon.svg"
             alt="Settings"
@@ -56,7 +56,7 @@ const Sidebar = (props: Props) => {
         </Button>
       </div>
       <div className="mt-96 space-y-10">
-        <Button className="space-x-3 bg-primary-100 rounded-lg w-40 h-12">
+        <Button className="h-12 w-40 space-x-3 rounded-lg bg-primary-100">
           <Image
             src="/assets/icons/logout-icon.svg"
             alt="Logout"
